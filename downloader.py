9f617b08bc5bd42c3a7b5bcc41d960543e306dca
ZirconiaCubed3v2 <lucian.adzima@gmail.com>
2024-11-10 14:53:32 -0800

    Removed 'outfiles' folder creation and use

--- downloader.py
+++ downloader.py
@@ -1,11 +1,7 @@
 import os
-try:
-    os.mkdir("outfiles")
-except FileExistsError:
-    pass
 epList = open("episodeslist.txt", 'r')
 lines = epList.readlines()
 for x in range(0, len(lines)):
     if ("mp4" in lines[x].strip()[-3:]):
         continue
-    os.system("wget \"%s\" -O \"outfiles/%s\"" % (lines[x].strip(), lines[x - 1].strip()))
+    os.system("wget \"%s\" -O \"%s\"" % (lines[x].strip(), lines[x - 1].strip()))
